68ec21c9b00a0d955d2fbcc359195d9ddf4c6ec9
yubzz09 <ayubbudie02@gmail.com>
2023-10-19 23:25:30 +0700

    upload

---
+++ src/scripts/component/nav-bar.js
@@ -0,0 +1,172 @@
+class NavBar extends HTMLElement {
+  constructor() {
+    super();
+    this.shadowDOM = this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  set clickEvent(event) {
+    this._clickEvent = event;
+    this.render();
+  }
+
+  get value() {
+    return this.shadowDOM.querySelector("#searchElement").value;
+  }
+
+  render() {
+    this.shadowDOM.innerHTML = `
+    <style>
+    * {
+      margin: 0;
+      padding: 0;
+    }
+    
+    :host {
+      position: -webkit-sticky;
+      position: sticky;
+      top: 0; /* Add this line to make it sticky at the top */
+      display: flex;
+      background-color: #461111;
+      justify-content: space-around;
+      height: 90px;
+      align-items: center;
+      color: rgb(167, 167, 236);
+    }
+    
+    .nav-input {
+      display: inline;
+      margin: 10px;
+      width: 40%;
+    }
+    
+    button {
+      width: 50px;
+      height: 44px;
+      cursor: pointer;
+    }
+    
+    button:hover {
+      background: #b3541e;
+      color: white;
+    }
+
+    .nav-list.open {
+      transform: translate(0, 0);
+    }
+    
+    input {
+      width: 150px;
+      height: 44px;
+    }
+    
+    ul {
+      display: flex;
+      list-style: none;
+      width: 40%;
+      justify-content: space-between;
+    }
+    
+    ul li a {
+      color: white;
+      text-decoration: none;
+      font-size: 35px;
+    }
+    
+    ul li a:hover {
+      color: #b3541e;
+    }
+
+    
+    .menu {
+      display: none;
+      flex-direction: column;
+      width: 44px;
+      height: 44px;
+      justify-content: space-between;
+      font-size: 30px;
+      color: #461111;
+    }
+    
+    /* responsive */
+    @media screen and (max-width: 768px) {
+      input {
+        width: 100px;
+        height: 44px;
+      }
+      ul {
+        width: 70%;
+      }
+    }
+
+    @media screen and (max-width: 1024px) {
+
+      ul {
+        width: 70%;
+      }
+    }
+    
+    @media screen and (max-width: 576px) {
+      .menu {
+        display: flex;
+      }
+      ul {
+        position: absolute;
+        right:  0;
+        top: 0;
+        height: 100vh;
+        align-items: center;
+        justify-content: space-evenly;
+        flex-direction: column;
+        background-color: #461111;
+        z-index: -2;
+        transform: translateX(100%);
+      }
+    
+      ul.slide {
+        transform: translateX(0);
+      }
+    }
+  </style>
+
+  <div class="nav-input">
+  <input placeholder="Search Restaurant" id="searchElement" type="search">
+  <button id="searchButtonElement" type="submit">Search</button>
+  </div>
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#favorite">Favorite</a></li>
+    <li><a href="https://www.linkedin.com/in/ayub09/">About Us</a></li>
+  </ul>
+  <button class="menu" >
+    ☰
+  </button>
+
+        `;
+
+    this.shadowDOM
+      .querySelector("#searchButtonElement")
+      .addEventListener("click", this._clickEvent);
+
+    const hamburgerButtonElement = this.shadowDOM.querySelector(
+      "#searchButtonElement"
+    );
+    const drawerElement = this.shadowDOM.querySelector(".nav-input");
+    hamburgerButtonElement.addEventListener("click", (event) => {
+      drawerElement.classList.toggle("open");
+      event.stopPropagation();
+    });
+
+    const menu = this.shadowDOM.querySelector(".menu");
+    const nav = this.shadowDOM.querySelector("ul");
+
+    menu.addEventListener("click", function () {
+      nav.classList.toggle("slide");
+    });
+  }
+}
+
+customElements.define("nav-bar", NavBar);
---
+++ src/scripts/index.js
@@ -0,0 +1,7 @@
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.css";
+import "./component/app-hero.js";
+import main from "./view/main.js";
+
+console.log("Hello Coders! :)");
+document.addEventListener("DOMContentLoaded", main);
