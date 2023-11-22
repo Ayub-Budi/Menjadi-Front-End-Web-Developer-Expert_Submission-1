b4c6988f0080477cebcd5d5eea857fc535df9d1e
yubzz09 <ayubbudie02@gmail.com>
2023-11-22 16:34:09 +0700

    submission-2 selesai

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,7 +1,6 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
-  CACHE_NAME: 'RestoCatalogue-V1',
   DATABASE_NAME: 'restaurants-catalogue-database',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restaurants',
